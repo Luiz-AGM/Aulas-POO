df7907153995a77f66123aac3c1bd2993458e299
Luiz-AGM <luiz_agm@hotmail.com>
2022-03-10 15:47:03 -0300

    atts

---
+++ Aula 02/Exercicio2.1/script.js
@@ -0,0 +1,7 @@
+console.log ("=== Exercicio 2.1===")
+
+// Questão #1
+
+// Crie uma classe CNH que tenha como propriedades país e idade que, por padrão, deve ser maior ou 
+// igual a 18, 16 se for igual a US ou CA e 21 se corresponder a CH ou RU. Se o país for BR deve 
+// também ser informada a categoria da carteira (A, B, C, D, E, AB, AC, AD ou AE).
---
+++ Aula 02/Exercicio2/script.js
@@ -0,0 +1,28 @@
+console.log ("=== Aula 2 ===")
+
+// Questão #1
+
+// Crie um objeto do tipo curso que tenha os seguintes atributos:
+
+// nome
+// p1
+// p2
+// media
+// Usando o setter insira o nome do curso, p1 e p2. Para a média considere a seguinte condição: Se, nota 
+// da p1 for 0, retorne a mensagem: "A nota da p1 foi 0, não vai ser possível fazer uma média" Se, nota 
+// da p1 maior que 0, faça o seguinte calculo: p1 + p2 / 2
+
+// Crie um get que retorne uma mensagem informando o nome do curso e sua média.
+
+
+// Questão #2
+
+// Faça um objeto TV com as propriedades canal, volume e ligada e métodos liga, desliga, 
+// mudaDeCanal e aumentaVolume, diminuiVolume.]
+
+
+// Questão #3
+
+// Refaça o exercício da TV utilizando uma função construtora
+
+// Refaça o exercício da TV utilizando a sintaxe de classe (class).
